3db98e885abac336f5462f1f14423bb3f1b1b6a9
Jordan Reel <jordanreel@me.com>
2025-08-15 22:52:55 -0400

    swap CameraDriverLabel and ComputeLabel

--- src/compute_render_setup.rs
+++ src/compute_render_setup.rs
@@ -17,5 +17,5 @@ pub fn compute_render_setup(world: &mut World) {
 	let (mut render_graph, sequence) = system_state.get_mut(world);
 
 	render_graph.add_node(ComputeLabel, ComputeNode::new(&sequence));
-	render_graph.add_node_edge(ComputeLabel, CameraDriverLabel);
+	render_graph.add_node_edge(CameraDriverLabel, ComputeLabel);
 }
